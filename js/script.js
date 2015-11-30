09b1ef5beb733a7e92ab4756f540a461c8bb5f1f
Raihan <raihan.dk@gmail.com>
2015-11-30 17:53:18 +0600

    Employee info and org info updated

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 var cardApp = angular.module('cardApp', ['naif.base64'])
 
 	.controller('cardCreatorController', ['$scope', function($scope){
+		$scope.info = {};
 		$scope.logo = 'LOGO';
+
+		$scope.info.fullName = 'Mr. Full Name Holder';
+		$scope.info.jobTitle = 'Some Designation';
+		$scope.info.phone = '01234 123 456';
+		$scope.info.email = 'employee@somecompany.com';
+		$scope.info.orgName = 'Some Company';
+		$scope.info.orgWeb = 'www.somecompany.com';
+		$scope.info.orgAddress = '11/2, Some Street,\nDhaka, Bangladesh';
 	}])
 
 	.directive('cardFront', [function () {
