207ac60f3e2005b8853148a5d0a409818ae115b2
Raihan <raihan.dk@gmail.com>
2015-11-30 18:43:19 +0600

    QR updated, not tested

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var cardApp = angular.module('cardApp', ['naif.base64'])
+var cardApp = angular.module('cardApp', ['naif.base64', 'ja.qr'])
 
 	.controller('cardCreatorController', ['$scope', function($scope){
 		$scope.info = {};
@@ -11,6 +11,16 @@ var cardApp = angular.module('cardApp', ['naif.base64'])
 		$scope.info.orgName = 'Some Company';
 		$scope.info.orgWeb = 'www.somecompany.com';
 		$scope.info.orgAddress = '11/2, Some Street,\nDhaka, Bangladesh';
+
+		$scope.qrcodeString = 'BEGIN:VCARD'
+			+ '\nN:' + $scope.info.fullName
+			+ '\nORG:' + $scope.info.orgName
+			+ '\nTITLE:' + $scope.info.jobTitle
+			+ '\nTEL:' + $scope.info.phone
+			+ '\nEMAIL:' + $scope.info.email
+			+ '\nURL:' + $scope.info.orgWeb
+			+ '\nADR:;;' + $scope.info.orgAddress
+			+ '\nEND:VCARD';
 	}])
 
 	.directive('cardFront', [function () {
